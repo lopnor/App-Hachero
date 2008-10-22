c6978e7cae023ae6d55559b247dae669093fb0f9
lopnor <lopnor@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-22 03:43:11 +0000

    lang/perl/App-Hachero: Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(リビジョン 21825)
    +++ Changes	(作業コピー)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension App::Hachero
     
    +0.03	2008-10-22T12:27:15+0900
    +
    +	- added count_up mode in Output::DBIC
    +	- skips line when parsing error happens in Parse::HadoopReduce
    +
     0.02	2008-10-19T14:56:35+0900
     
     	- requires plugins in plugin_path on HadoopReduce
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-Hachero/trunk@21826 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/Hachero.pm
+++ lib/App/Hachero.pm
@@ -2,7 +2,7 @@ package App::Hachero;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use Class::Component;
 use base qw(Class::Accessor::Fast);
 use UNIVERSAL::require;
