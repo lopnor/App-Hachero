664efd856e0d935a6cae57a4c23f3cca094f1013
lopnor <lopnor@d0d07461-0603-4401-acd4-de1884942a52>
2009-04-05 19:52:36 +0000

    dev release
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-Hachero/trunk@31961 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/Hachero.pm
+++ lib/App/Hachero.pm
@@ -2,7 +2,7 @@ package App::Hachero;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.06';
+our $VERSION = '0.06_01';
 use Class::Component;
 use base qw(Class::Accessor::Fast);
 use UNIVERSAL::require;
